0e3d384608bc7c9511ff5dd9b3552cb7c65523ca
vsitey <512272538@qq.com>
2019-12-23 16:21:20 +0800

    wip

---
+++ hexo-demo/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+program_name=hexo-demo
+program_version=$(date "+%Y%m%d%H%M%S")
+program_port=4000
+
+image_name=${program_name}:${program_version}
+
+echo $image_name
+
+docker build -t ${image_name} .
+
+remove_images=$(docker ps -a -q -f name=/${program_name}_)
+remove_images_length=${#remove_images[n]}
+echo remove images list: ${remove_images}, length: $remove_images_length
+
+if [ ${remove_images_length} -gt 0 ]; then
+  echo removing...
+  docker rm -f ${remove_images}
+fi
+
+docker run -d --name=${program_name}_${program_version} -p ${program_port}:4000 ${image_name}
