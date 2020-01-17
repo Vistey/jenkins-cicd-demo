ad2d223f14ca5fd9117a630ed6c93f3eab044244
vsitey <512272538@qq.com>
2020-01-17 13:17:39 +0800

    wip

--- vuepress/test/home.js
+++ vuepress/test/home.js
@@ -1,8 +1,10 @@
 import { Selector } from 'testcafe';
 
-fixture `Getting Started`
-    .page `http://localhost:80/testcafe/example`;
+fixture('Getting Started').page('http://192.168.56.102:8080/')
 
 test('My first test', async t => {
     // Test code
+    console.log("this is case 1");
+    const helloWorld = Selector('#hello-world').exists;
+    await t.expect(helloWorld).ok();
 });
